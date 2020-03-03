c93d62287d4b843dcb476b68cea9238f470483d7
Andrey Moskvin <andrey.moskvin@sigmaukraine.com>
2020-03-03 15:01:12 +0200

    Minor config changes

--- server-less/config-stack/lib/config-stack-stack.ts
+++ server-less/config-stack/lib/config-stack-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from '@aws-cdk/core'
-import * as s3 from '@aws-cdk/aws-s3'
+// import * as s3 from '@aws-cdk/aws-s3'
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as apigateway from '@aws-cdk/aws-apigateway'
 import { LayerVersion } from '@aws-cdk/aws-lambda';
@@ -11,10 +11,11 @@ export class ConfigStackStack extends cdk.Stack {
     // const bucket = new s3.Bucket(this, "SwiftLayers");
     
     const layer = LayerVersion.fromLayerVersionArn(this, 'Swift51', 'arn:aws:lambda:eu-central-1:721126196132:layer:Swift51:4')
+    
     const handler = new lambda.Function(this, 'ConfigHandler',
     {
       runtime: lambda.Runtime.PROVIDED,
-      code: lambda.Code.asset('../config/lambda.zip'),
+      code: lambda.Code.fromAsset('../config/lambda.zip'),
       handler: 'lambda.lambda',
       layers: [layer]
     })
