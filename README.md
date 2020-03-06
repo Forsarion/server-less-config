8587663f6f702aa70cae96c491c988cc23fd9d5f
Andrey Moskvin <andrey.moskvin@sigmaukraine.com>
2020-03-06 13:58:48 +0200

    Readme update

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Requirements
 - Amazon Web Services Account
+- `brew install aws-cli`
 - `brew install aws-cdk`
 - Xcode 11 (only for iOS app)
 - VS Code and AWS Toolkit extension
+
+# Monorepo structure
+
+`frontend/ConfigDemo` - iOS app written using SwiftUI
+
+`server-less/config` - Lambda code written in Swift
+
+`server-less/config/core` - Shareable code between iOS app and Lambda
+
+`server-less/config/lambda` - Lambda code itself
+
+`server-less/config-stack` - AWS CDK project that allows to setup cloud with one command 
--- server-less/config-stack/lib/config-stack-stack.ts
+++ server-less/config-stack/lib/config-stack-stack.ts
@@ -1,25 +1,32 @@
 import * as cdk from '@aws-cdk/core'
-// import * as s3 from '@aws-cdk/aws-s3'
+import * as s3 from '@aws-cdk/aws-s3'
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as apigateway from '@aws-cdk/aws-apigateway'
-import { LayerVersion } from '@aws-cdk/aws-lambda';
+import * as s3deploy from '@aws-cdk/aws-s3-deployment'
+import { LayerVersion, Code } from '@aws-cdk/aws-lambda';
 import { EmptyModel } from '@aws-cdk/aws-apigateway';
 
 export class ConfigStackStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // const bucket = new s3.Bucket(this, "SwiftLayers");
+    const bucket = new s3.Bucket(this, 'swift-layers');
+    new s3deploy.BucketDeployment(this, 'swift-layers-deployment', {
+      sources: [s3deploy.Source.asset('../config/swift-lambda-runtime.zip.zip')],
+      destinationBucket: bucket,
+      destinationKeyPrefix: '/swift-lambda-runtime'
+    })
     
+    // const layer = new LayerVersion(this, 'swift-51', { code: Code.fromBucket() })
     const layer = LayerVersion.fromLayerVersionArn(this, 'Swift51', 'arn:aws:lambda:eu-central-1:721126196132:layer:Swift51:4')
-    
-    const handler = new lambda.Function(this, 'ConfigHandler',
-    {
+        
+    const handler = new lambda.Function(this, 'ConfigHandler', {
       runtime: lambda.Runtime.PROVIDED,
       code: lambda.Code.fromAsset('../config/lambda.zip'),
       handler: 'lambda.lambda',
       layers: [layer]
     })
+    bucket.grantRead(handler)
 
     const api = new apigateway.LambdaRestApi(this, 'config-handler-api', {
       handler,
