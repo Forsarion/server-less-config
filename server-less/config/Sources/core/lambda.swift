22d4bfdd65aa1adf9b9f2dd203575c12b943c2b3
Andrii Moskvin <berkold90@gmail.com>
2020-03-07 11:40:43 +0200

    Updated scripts and lambda for one command deploy

--- server-less/config/Sources/core/lambda.swift
+++ server-less/config/Sources/core/lambda.swift
@@ -1,7 +1,18 @@
 import AWSLambdaSwift
 
 public func config(input: Input, context: Context) -> Output {
-    Output(environment: input.environment,
-           auth: Url(value: "https://auth-url.com"),
-           api: Url(value: "https://api-url.com"))
+    switch input.environment.value {
+    case "dev":
+        return Output(environment: input.environment,
+                      auth: Url(value: "https://dev.auth.com"),
+                      api: Url(value: "https://dev.api.com"))
+    case "prod":
+        return Output(environment: input.environment,
+                      auth: Url(value: "https://prod.auth.com"),
+                      api: Url(value: "https://prod.api.com"))
+    default:
+        return Output(environment: input.environment,
+                      auth: Url(value: "https://auth-url.com"),
+                      api: Url(value: "https://api-url.com"))
+    }
 }
